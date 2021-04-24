d33418dc00df816c1433a16960ee8eff6868a4ee
Bing35 <69435914+Bing35@users.noreply.github.com>
2021-04-24 00:54:17 -0400

    implemented navigation and search and styled page

--- mechanics/static/mechanics/jsx/react.jsx
+++ mechanics/static/mechanics/jsx/react.jsx
@@ -44,12 +44,89 @@ function App(props) {
 
 }
 
-
-
 function Nav(props) {
+    const [viewport, setViewport] = useState(undefined)
+
+    function render() {
+
+        if (viewport === undefined) {
+            return 'undefined'
+        }
+        else if (viewport === 'sm') {
+            return (
+                <nav>
+                    <button onClick={showNav} className='btn btn-secondary'>Menu</button>
+                    <div>
+                        <ul className='nav flex-column'>
+                            <li className='nav-item'><a className='nav-link'><Search
+                                setMonsterId={props.setMonsterId}
+                                showNav={showNav}
+                                viewport={viewport}
+                            /></a></li>
+                            <li className="nav-item"><a className='nav-link'>About Us</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            )
+        }
+        else {
+            return (
+                // <nav className='navbar navbar-expand-sm bg-secondary navbar-dark'>
+                <nav>
+                    <ul className='nav'>
+                        <li className='nav-item'><a className='nav-link'><Search
+                            setMonsterId={props.setMonsterId}
+                            showNav={() => null}
+                            viewport={viewport}
+                        /></a></li>
+                        <li className="nav-item"><a className='nav-link'>About Us</a></li>
+                    </ul>
+                </nav>
+            )
+        }
+
+    }
+
+    // initialize display
+    useEffect(function () {
+        const navDiv = document.querySelector('nav>div')
+        if (navDiv !== null) {
+            document.querySelector('nav>div').style.display = 'none';
+        }
+    }, [viewport])
+
+    // viewport changes
+    useEffect(function () {
+        changeViewport()
+    }, [])
+
+    useEffect(function () {
+        document.querySelector('body').onresize = changeViewport
+    })
+
+
+    function changeViewport() {
+        if (window.innerWidth < 1000) {
+            if (viewport !== 'sm') {
+                setViewport('sm')
+            }
+        }
+        else {
+            if (viewport !== 'lg') {
+                setViewport('lg')
+            }
+        }
+    }
+
+
+
+    return render()
+}
+
+function Search(props) {
     const [search, setSearch] = useState('')
     const [monsters, setMonsters] = useState(undefined)
-    let monsterList = []
+    const [monsterList, setMonsterList] = useState([])
     const invalidInput = [{
         'name': 'invalid input',
         'id': 1
@@ -60,7 +137,7 @@ function Nav(props) {
         return (
             <div>
                 <input type="text" onChange={changeSearch} value={search} />
-                <ul>
+                <ul id='monster-list'>
                     {monsterList}
                 </ul>
             </div>
@@ -91,20 +168,32 @@ function Nav(props) {
 
     }, [search])
 
+    // set monsterList
+    useEffect(function () {
+        if (monsters === undefined) {
+            setMonsterList([])
+            console.log('monster undefined');
+            document.querySelector('#monster-list').style.display = 'none'
+        }
+        else {
+            console.log('monster = monsterlist');
+            setMonsterList(monsters.map(item => (
+                <li key={item.name} ><button onClick={setMonsterId} data-id={item.id} className='btn btn-light'>{item.name}</button></li>
+            )))
+            if (props.viewport === 'lg') {
+                document.querySelector('#monster-list').style.display = 'flex'
+            }
+            else {
+                document.querySelector('#monster-list').style.display = 'block'
+            }
+        }
+    }, [monsters])
+
 
     function setMonsterId(e) {
         props.setMonsterId(e.target.dataset.id)
         setSearch('')
-    }
-
-
-    if (monsters === undefined) {
-        monsterList = []
-    }
-    else {
-        monsterList = monsters.map(item => (
-            <li key={item.name} ><button onClick={setMonsterId} data-id={item.id}>{item.name}</button></li>
-        ))
+        props.showNav()
     }
 
     function changeSearch(e) {
@@ -121,6 +210,12 @@ function Title(props) {
 }
 
 function Stats(props) {
+    // generate spam text
+    const list = []
+    for (let i = 0; i < 50; i++) {
+        list.push('this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.this is some text.')
+    }
+
     return (
         <div id='stats' className='row'>
             {/* parts dmg sever, blunt, range */}
@@ -160,11 +255,28 @@ function Stats(props) {
                 </tbody>
             </table>
 
+            {/* spam text */}
+            {/* {list.map((p, index) => (
+                <p key={index}>
+                    {p}
+                </p>
+            ))} */}
+
         </div>
     )
 }
 
-
+function showNav() {
+    const style = document.querySelector('nav>div').style
+    if (style.display !== 'none') {
+        style.display = 'none'
+        style.backgroundColor = 'white'
+    }
+    else {
+        style.display = 'block'
+        style.backgroundColor = 'lightgray'
+    }
+}
 
 
 ReactDOM.render(<App />, document.querySelector('#root'))
